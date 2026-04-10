d8f19d1f88065b6b8f1c6144c04894ae963ba55e
Marcel Deluca <andalusi.private@gmail.com>
2026-04-10 15:13:42 +0200

    Created CalendarUser entity

---
+++ backend/src/main/java/se/andaluscalendar/backend/model/CalendarUser.java
@@ -0,0 +1,24 @@
+package se.andaluscalendar.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "calendar_users")
+@Data
+@NoArgsConstructor
+public class CalendarUser {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(name = "selected_city")
+    private String selectedCity;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+}
